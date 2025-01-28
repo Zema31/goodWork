63c2131b012d21cbfe223ff41b9fc5a8767e9c4e
Zema31 <zemfiraharisova@yandex.ru>
2025-01-29 01:32:53 +0300

    migrations

---
+++ database/migrations/2025_01_28_221014_create_promos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('text');
+            $table->string('status');
+            $table->string('url');
+            $table->integer('view_counts');
+            $table->decimal('cpm', total: 10, places: 2);
+            $table->decimal('amount', total: 10, places: 2);
+            $table->string('button_text');
+            $table->foreignId('company_id')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promos');
+    }
+};
