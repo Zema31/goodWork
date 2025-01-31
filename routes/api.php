386d8bdc8ea8a818b5bb2a661fe6cb3717b03979
Zema31 <zemfiraharisova@yandex.ru>
2025-01-31 04:15:16 +0300

    Add Promo

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,21 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CompanyController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\PromoController;
+use App\Http\Controllers\Api\AmountController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('signup', [AuthController::class, 'signup']);
+});
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::get("/test-me", function () {
-//     return 'Hello from Laravel!';
-// });
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/signup', [AuthController::class, 'signup']);
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::group(['prefix' => 'auth'], function () {
+        Route::get('logout', [AuthController::class, 'logout']);
+        Route::delete('delete', [AuthController::class, 'delete']);
+    });
     Route::apiResource('company', CompanyController::class);
+    Route::apiResource('promo', PromoController::class);
+    Route::apiResource('amount', AmountController::class);
 });
